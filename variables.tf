ec070aaf30d4f891381e7accdfadb1be2cdb6e1e
Chris Roy <chris.roy@tamr.com>
2020-10-12 14:14:33 -0400

    Adding private ip support

--- variables.tf
+++ variables.tf
@@ -3,6 +3,12 @@ variable "name" {
   description = "name of the cloud sql instance to create"
 }
 
+variable "activation_policy" {
+  type        = string
+  description = "This specifies when the instance should be active. Can be either ALWAYS, NEVER or ON_DEMAND"
+  default     = "ALWAYS"
+}
+
 variable "project_id" {
   type        = string
   description = "the project to create the sql instance in"
@@ -24,6 +30,12 @@ variable "disk_type" {
   type        = string
   description = "The disk type to use on the instance. should be either PD_SSD or PD_STANDARD"
   default     = "PD_SSD"
+}
+
+variable "disk_autoresize" {
+  type        = bool
+  description = "Enable disk to autoresize itself"
+  default     = true
 }
 
 variable "labels" {
@@ -55,4 +67,34 @@ variable "cloud_sql_admin_members" {
   type        = list(string)
   description = "The list of members to bind to sql admin role"
   default     = []
+}
+
+variable "backup_enabled" {
+  type        = bool
+  description = "True if backup configuration is enabled."
+  default     = false
+}
+
+variable "backup_start_time" {
+  type        = string
+  description = "HH:MM format time indicating when backup configuration starts."
+  default     = "06:00"
+}
+
+variable "backup_point_in_time_recovery_enabled" {
+  type        = bool
+  description = "True if Point-in-time recovery is enabled. Will restart database if enabled after instance creation."
+  default     = false
+}
+
+variable "enable_private_ip" {
+  type        = bool
+  description = "Whether this instance should use a public or private ip address"
+  default     = false
+}
+
+variable "private_network_id" {
+  type        = string
+  description = "The VPC network from which the Cloud SQL instance is accessible for private IP"
+  default     = ""
 }
