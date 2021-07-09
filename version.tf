158fa6fe01e252f0066921d636656205cfa3ca40
Apeksha Khilari <apeksha.khilari@sjsu.edu>
2021-07-09 12:19:10 -0700

    SRE-1015: Update terraform to 0.14 version

--- version.tf
+++ version.tf
@@ -1,6 +1,13 @@
 terraform {
-  required_version = ">= 0.12"
+  required_version = ">= 0.14"
   required_providers {
-    google = ">= 3.18.0, <= 4.0.0"
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 3.18.0, <= 4.0.0"
+    }
+    postgresql = {
+      source  = "cyrilgdn/postgresql"
+      version = ">=1.12.0"
+    }
   }
 }
