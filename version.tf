8dad418006f254d0f53cde96bfe2ad1cba2ab159
Brett Lentz <blentz@users.noreply.github.com>
2022-01-28 11:12:56 -0500

    DSD-760: MINOR: upgrade google provider version. (#27)

--- version.tf
+++ version.tf
@@ -1,9 +1,9 @@
 terraform {
-  required_version = ">= 0.14"
+  required_version = ">= 1.0"
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">= 3.18.0, <= 4.0.0"
+      version = ">= 4.6.0"
     }
   }
 }
